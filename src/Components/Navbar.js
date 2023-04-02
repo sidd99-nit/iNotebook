0eeb11d1c22bcd91c44eff326ca85b2ba9659613
Siddharth Kumar <siddharthkumar726@gmail.com>
2023-04-02 12:02:32 +0530

    Link to anchor

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,10 +13,10 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
+            <a className="nav-link" href="/about">About</a>
           </li>
         </ul>
         {/* <form className="d-flex" role="search">
